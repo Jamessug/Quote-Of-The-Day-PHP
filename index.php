85c67949144bfe81c1fb57ab2bbe3b5dcd08d1d8
Jamessug <jamessug@icloud.com>
2024-08-23 17:25:49 +0100

    M2 - Discovered array_rand, built in function that picks a random index from the array. They made random_quote variable that takes the chosen quote. Then embedded the php variable inside the html and added some boostrap classes.

--- index.php
+++ index.php
@@ -9,9 +9,15 @@ $quotes = ["The sea's a cruel mistress, but she has her moments.",
 "If you think nobody cares if you're alive, try missing a couple of payments.",
 "Always remember that you are unique, just like everyone else.",
 "Dance like nobody's watching, because they're probably checking their phones.",
-"Life's too short to be serious all the time, so if you can't laugh at yourself, call me—I'll laugh at you.",
+"Life's too short to be serious all the time, so if you can't laugh at yourself, call me - I'll laugh at you.",
 "The road to success is dotted with many tempting parking spaces.",
 "You can't have everything. Where would you put it?",
 "When in doubt, make a fool of yourself. There is a microscopically thin line between being brilliantly creative and acting like the most gigantic idiot on earth. So what the hell, leap!",
 "I'm not arguing. I'm just explaining why I'm right.",
 "The universe is under no obligation to make sense to you."];
+
+$random_key = array_rand($quotes); // array_rand picks random index from array
+
+$random_quote = $quotes[$random_key];
+
+echo $random_quote;
